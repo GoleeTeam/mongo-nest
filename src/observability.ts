e5f6ce3f2977ba15423552d802a3e2830b6fb3e8
Jacopo Franzoi <jacopo@golee.it>
2026-01-12 09:17:46 +0100

    chore: observability: aligned to otel semver

--- src/observability.ts
+++ src/observability.ts
@@ -1,7 +1,16 @@
 import { MetricService } from '@golee/nestjs-otel';
 import { Logger } from '@nestjs/common';
 import { Attributes, Histogram, ValueType } from '@opentelemetry/api';
+import {
+    ATTR_DB_OPERATION_NAME,
+    ATTR_DB_SYSTEM,
+    ATTR_ERROR_TYPE,
+    ATTR_SERVER_ADDRESS,
+    METRIC_DB_CLIENT_OPERATION_DURATION,
+} from '@opentelemetry/semantic-conventions/incubating';
 import { CommandFailedEvent, CommandSucceededEvent, MongoClient, MongoClientOptions } from 'mongodb';
+
+const DB_SYSTEM_VALUE_MONGODB = 'mongodb' as const;
 
 export class Observability {
     private logger = new Logger(Observability.name);
@@ -14,14 +23,20 @@ export class Observability {
             return;
         }
 
-        this.operationDuration = this.metricService.getHistogram('db.client.operation.duration', {
-            description: 'Duration of database client operations.',
-            unit: 's',
-            valueType: ValueType.DOUBLE,
-            advice: {
-                explicitBucketBoundaries: [0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10],
+        // see:
+        // - reference: https://github.com/open-telemetry/semantic-conventions/blob/v1.26.0/docs/database/database-metrics.md#metric-dbclientoperationduration
+        // - sample: https://github.com/open-telemetry/opentelemetry-js-contrib/blob/main/packages/instrumentation-pg/src/instrumentation.ts
+        this.operationDuration = this.metricService.getHistogram(
+            METRIC_DB_CLIENT_OPERATION_DURATION, //'db.client.operation.duration',
+            {
+                description: 'Duration of database client operations.',
+                unit: 's',
+                valueType: ValueType.DOUBLE,
+                advice: {
+                    explicitBucketBoundaries: [0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1, 5, 10],
+                },
             },
-        });
+        );
     }
 
     public enableOn(mongoOptions: MongoClientOptions) {
@@ -43,7 +58,7 @@ export class Observability {
             .on('commandFailed', (event: CommandFailedEvent) => {
                 this.operationDuration!.record(this.toSeconds(event.duration), {
                     ...this.describe(event),
-                    'error.type': event.failure.name,
+                    [ATTR_ERROR_TYPE]: event.failure.name,
                 });
             });
     }
@@ -54,9 +69,9 @@ export class Observability {
 
     private describe(event: CommandSucceededEvent | CommandFailedEvent) {
         return {
-            'db.system': 'mongodb',
-            'server.address': event.address,
-            'db.operation.name': event.commandName,
+            [ATTR_DB_SYSTEM]: DB_SYSTEM_VALUE_MONGODB,
+            [ATTR_SERVER_ADDRESS]: event.address,
+            [ATTR_DB_OPERATION_NAME]: event.commandName,
         };
     }
 }
